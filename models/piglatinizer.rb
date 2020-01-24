e65998e7b0f69f8920fcdf0fe16082dcdf9847b6
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-01-24 18:23:51 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,17 +3,6 @@ class PigLatinizer
   def initialize
   end
   
-  # def piglatinize(phrase)
-  #   if phrase.start_with?((/[bcdfghjklmnpqrstvwxyz]/))
-  #   phrase_array = phrase.split("")
-  #   phrase_array = phrase_array.rotate(1)
-  #   piglatin = phrase_array.join + "ay"
-  #   piglatin
-  #   else
-  #   word + "way" 
-  #   end
-  # end
-  
   def piglatinize(string)
     split_words = string.split(" ")
     pig_split_words = split_words.map {|word| piglatinize_it(word)}
